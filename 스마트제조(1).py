a11c9cd8fca1b180383e82ef31e36ce01ddb5b7d
ppvid <ksm7848@gmail.com>
2025-04-25 15:42:24 +0900

    문제 풀기

--- 스마트제조(1).py
+++ 스마트제조(1).py
@@ -1,9 +1,11 @@
+#1.1 장난감 공장의 단일 제품 생산 계획 문제 풀이
+
 from pyomo.environ import *
 #Abstract Model(추상 모델): 수식으로 표현된 모델
 #Concrete Model(확정 모델): 추상 모델에 구체적인 수치를 대입한 모델
 #문제를 풀 시에는 계수가 확정된 모델인 확정 모델을 사용한다.
 model=ConcreteModel()
-#결정변수 x를 정의 (domain=NonNegativeReals)은 비음 조건을 나타냄냄
+#결정변수 x를 정의 (domain=NonNegativeReals)은 비음 조건, 실수 변수 설정함. 
 model.x= Var(domain=NonNegativeReals)
 #Objective( expr = 결정변수x와 계수값 넣어서 목적함수를 정의, sense = 최대화/최소화 결정)
 #expr(expression, 수식을 의미)
@@ -13,7 +15,9 @@ model.profit=Objective(
     sense = maximize
 )
 #Constrant(expr = 제약식 정의)
+#수요 제약식
 model.demand = Constraint(expr = model.x <= 40)
+#가용시간 제약식 
 model.laborA = Constraint(expr = model.x <= 80)
 model.laborB = Constraint(expr = 2*model.x <=100)
 #.pprint: 모델 안의 변수, 목적함수, 제약식을 확인할 수 있는 출력 함수수
@@ -26,3 +30,40 @@ SolverFactory('glpk').solve(model).write()
 
 model.profit.display() #목적함수 해 출력 
 model.x.display() #결정 변수 해 출력 
+#출력값에서  변수 선언 부분 Fixed는 고정값인지 변수값인지 나타냄
+#fixed: False > 변수
+#fixed: Ture > 상수
+#변수선언부분 stale은 갱신된 값인지 아닌지를 나타냄
+#stale : True > 변수의 값이 최신 계산 결과가 아님
+#stale : False > 변수의 값이 갱신된 값임
+
+#Solver Results 해석
+#Number of nonzeros : 0이 아닌 계수의 수 (희소 행결(sparse matrix)일 경우 중요함)
+#Solver information 해석
+#status : OK > 정상적으로 계산 완료되었다.
+#branch and bound : 가지치기 기법을 사용했는지 확인 가능
+
+print(f'최대이익 = {model.profit()} 천원/주')
+print(f'x의 최적 생산량 = {model.x()} 개/주')
+
+#1.2 장난감 공장의 단일 제품 생산 계획 문제 풀이_제품 Y 추가
+from pyomo.environ import *
+model2 = ConcreteModel()
+model2.x= Var(domain=NonNegativeReals)
+model2.y= Var(domain=NonNegativeReals)
+
+model2.profit = Objective(
+    expr= 270*model2.x-(100*model2.x+50*model2.x+40*(2*model2.x)) + 210*model2.y-(90*model2.y+50*model2.y+40*model2.y),
+    sense=maximize
+)
+
+model2.demand = Constraint(expr=model2.x <= 40)
+model2.demand = Constraint(expr=model2.x+model2.y<=80)
+model2.labor = Constraint(expr = 2*model2.x+model2.y <= 100)
+
+SolverFactory('glpk').solve(model2).write()
+
+print(f'최대이익= {model2.profit()} 천원/주')
+print(f'x의 최적생산량 = {model2.x()} 개/주')
+print(f'y의 최적생산량= {model2.y()} 개/주')
+
